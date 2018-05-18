9f692a3f3496650f8e21b7524a3fa373b19ba23d
MathiasHaudgaard <mathiashaudgaard@gmail.com>
2018-05-18 13:58:11 +0200

    template generator for bio_conda recipes

---
+++ generate_recipe.py
@@ -0,0 +1,75 @@
+import yaml
+import requests
+import sys
+import hashlib
+import os
+
+
+bio_tools_pkg_url = 'https://www.bio.tools/api/tool'
+
+
+def getAttributes(metaData):
+    name = metaData['name']
+    version = metaData['version'] if metaData['version'] is not None else 1.0
+    url = metaData['download'][0] if len(metaData['download']) > 0 else ""
+    fn = url.split('/')[-1] if url != "" else ""
+    sha = ""
+    home = metaData['homepage']
+    summary = metaData['description']
+    biotool_id = metaData['id']
+    doi = ""
+    for pub in metaData['publication']:
+        if pub['doi'] is not None:
+            doi = pub['doi']
+            break
+
+    return [name, version, fn, url, sha, home, summary, biotool_id, doi]
+            
+    
+
+if __name__ == '__main__':
+
+    r = requests.get(bio_tools_pkg_url+'/' + sys.argv[1])
+    if r.status_code != 200:
+        print('Package does not exist')
+        sys.exit()
+
+    document = """package:
+    name: {0}
+    version: {1}  
+
+build:
+    number: 0
+
+source:
+    fn: {2}
+    url: {3}
+    sha256: {4}
+
+requirements:
+    build:
+        - gcc
+    run:
+        - libgcc
+
+test:
+    commands:
+        - {0} -h
+
+about:
+    home: {5}
+    license: file
+    license_file: COPYING
+    summary: {6}
+extra:
+    identifiers:
+        - biotools:{7}
+        - doi:{8}
+"""
+
+    document = document.format(*getAttributes(r.json()))
+    if not os.path.exists("recipes/" + sys.argv[1]):
+        os.makedirs("recipes/" + sys.argv[1])
+    
+    with open("recipes/"+sys.argv[1] + "/meta.yaml", 'w') as yaml_file:
+        yaml_file.write(document)
